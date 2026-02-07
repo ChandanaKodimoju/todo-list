8c9dcb0179a6c3c82a2c448d09fd020a3df543d7
Chandanakodimoju <https://github.com/ChandanaKodimoju>
2026-02-07 20:27:57 +0530

    todo list

--- script.js
+++ script.js
@@ -1,70 +1,92 @@
-let arr=[
-    "i want to eat food at 10am",
-    "i want to meet balayya bau at 12pm",
-    "i want to meet kcr at 10pm"
-]
+let tasks = [];
+let dates = [];      // changed from times
+let statusArr = [];
 
-
-function display(){
-    let trs="";
-    for(let ind in arr){
-        trs+=`
-            <tr>
-                <td>${arr[ind]}</td>
-                <td>
-                    <button onclick="deleteTask(${ind});">delete</button>
-                </td>
-                <td>
-                    <button onclick="editTask(${ind});">edit</button>
-                </td>
-            </tr>
-        `
+function display() {
+    let trs = "";
+    for (let ind in tasks) {
+        trs += `
+        <tr>
+            <td>${tasks[ind]}</td>
+            <td>${dates[ind]}</td>
+            <td>${statusArr[ind]}</td>
+            <td>
+                <button class="delete" onclick="deleteTask(${ind});">delete</button>
+           
+                <button class="edit" onclick="editTask(${ind});">edit</button>
+            </td>
+        </tr>
+        `;
     }
-    let refElem=document.getElementById("ref");
-    refElem.innerHTML=`
-        <table border="1px">
+    let refElem = document.getElementById("ref");
+    refElem.innerHTML = `
+        <table class="task-table">
+            <tr>
+                <th>Task</th>
+                <th>Date</th>   <!-- changed -->
+                <th>Status</th>
+                <th>Actions</th>
+                
+            </tr>
             ${trs}
         </table>
     `;
-   
 }
 
+function deleteTask(index) {
+    let rev = confirm("Do you want to delete??");
+    if (rev == false) {
+        return;
+    }
+    tasks.splice(index, 1);
+    dates.splice(index, 1);     // changed
+    statusArr.splice(index, 1);
+    display();
+    alert("Deleted Successfully!!!");
+}
 
-display();
+function addTask(e) {
+    e.preventDefault();
+    let allForms = document.forms;
+    let myFormElem = allForms.myForm;
 
+    let textBoxElem = myFormElem.task;
+    let task = textBoxElem.value;
 
-function deleteTask(index){
-    let rev=confirm("want to delete??");// confirmation from user 
-    if(rev==false){
+    let dateBoxElem = myFormElem.date;   // changed
+    let date = dateBoxElem.value;
+
+    let selected = "";
+    for (let r of myFormElem.status) {
+        if (r.checked) selected = r.value;
+    }
+
+    if (!selected) {
+        alert("Select status");
         return;
     }
 
+    tasks.push(task);
+    dates.push(date);     // changed
+    statusArr.push(selected);
 
-    arr.splice(index,1);
     display();
-    alert("deleted!!!");
+    myFormElem.reset();
+    alert("Task Added!!!");
 }
 
+function editTask(ind) {
+    let enteredTask = prompt("Edit Task: ", tasks[ind]);
+    let enteredDate = prompt("Edit Date: ", dates[ind]);   // changed
+    let enteredstatus = prompt("Edit status: ", statusArr[ind]);
 
-function addTask(e){
-    e.preventDefault();// to prevent refresh
-    let allForms=document.forms;// to get all forms
-    let myFormElem=allForms.myForm; // to get my form (name)
-    let textBoxElem=myFormElem.task;// to get textBox (name=task)
-    let task=textBoxElem.value;// get value from textbox
-    arr.push(task);
-    display();
-    textBoxElem.value="";// remove value form textBox
-    alert("added!!!");
-}
+    if (enteredTask == null || enteredDate == null || enteredstatus == null) {
+        return;
+    }
 
+    tasks[ind] = enteredTask;
+    dates[ind] = enteredDate;   // changed
+    statusArr[ind] = enteredstatus;
 
-
-
-function editTask(ind){
-   
-    let enteredTask = prompt(arr[ind]);// take input from user
-    arr[ind]=enteredTask;
     display();
 }
-
